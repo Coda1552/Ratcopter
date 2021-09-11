75b8129b7416fa9bbdf3bbc24b5741796dcce75f
George Kazanjian <soggymustacheofficialmods@gmail.com>
2021-09-11 19:49:30 -0400

    食べています

--- src/main/java/coda/ratcopter/Ratcopter.java
+++ src/main/java/coda/ratcopter/Ratcopter.java
@@ -1,7 +1,9 @@
 package coda.ratcopter;
 
+import coda.ratcopter.entities.RatcopterEntity;
 import coda.ratcopter.init.RatcopterItems;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -10,13 +12,19 @@ import org.apache.logging.log4j.Logger;
 
 @Mod(Ratcopter.MOD_ID)
 public class Ratcopter {
-    public static final String MOD_ID = "ratcopter";
-    public static final Logger LOGGER = LogManager.getLogger();
+	public static final String MOD_ID = "ratcopter";
+	public static final Logger LOGGER = LogManager.getLogger();
 
-    public Ratcopter() {
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-        IEventBus forgeBus = MinecraftForge.EVENT_BUS;
+	public Ratcopter() {
+		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+		IEventBus forgeBus = MinecraftForge.EVENT_BUS;
+		bus.addListener(Ratcopter::registerEntityAttributes);
 
-        RatcopterItems.ITEMS.register(bus);
-    }
+		RatcopterItems.ITEMS.register(bus);
+		RatcopterItems.ENTITIES.register(bus);
+	}
+
+	private static void registerEntityAttributes(EntityAttributeCreationEvent event) {
+		event.put(RatcopterItems.RATCOPTER_2.get(), RatcopterEntity.createBaseHorseAttributes().build());
+	}
 }
--- src/main/java/coda/ratcopter/init/RatcopterItems.java
+++ src/main/java/coda/ratcopter/init/RatcopterItems.java
@@ -1,6 +1,10 @@
 package coda.ratcopter.init;
 
 import coda.ratcopter.Ratcopter;
+import coda.ratcopter.entities.RatcopterEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityClassification;
+import net.minecraft.entity.EntityType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraftforge.fml.RegistryObject;
@@ -8,7 +12,16 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class RatcopterItems {
+    /*
+    Official executive comment: "LOL";
+     */
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Ratcopter.MOD_ID);
+    public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(ForgeRegistries.ENTITIES, Ratcopter.MOD_ID);
 
+    public static final RegistryObject<EntityType<RatcopterEntity>> RATCOPTER_2 = create("ratcopter", EntityType.Builder.of(RatcopterEntity::new, EntityClassification.CREATURE).sized(0.8f, 0.8f));
     public static final RegistryObject<Item> RATCOPTER = ITEMS.register("ratcopter", () -> new Item(new Item.Properties().tab(ItemGroup.TAB_TRANSPORTATION).stacksTo(1)));
+
+    private static <T extends Entity> RegistryObject<EntityType<T>> create(String name, EntityType.Builder<T> builder) {
+        return ENTITIES.register(name, () -> builder.build(Ratcopter.MOD_ID + "." + name));
+    }
 }
